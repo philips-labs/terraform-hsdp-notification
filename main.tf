478cd4f2dd96aca16b491b8ce02521a7bb0f9d85
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-05-13 08:48:33 +0200

    Add the correct permission

--- main.tf
+++ main.tf
@@ -35,8 +35,7 @@ resource "hsdp_iam_role" "producer_admin" {
     "NS_PRODUCER.CREATE",
     "NS_PRODUCER.READ",
     "NS_PRODUCER.DELETE",
-    # Below permission is missing in IAM but still referenced in the documentation
-    #"NS_PRODUCER_SCOPE.READ",
+    "NS_TOPIC_SCOPE.READ",
     "NS_TOPIC.CREATE",
     "NS_TOPIC.READ",
     "NS_TOPIC.UPDATE",
